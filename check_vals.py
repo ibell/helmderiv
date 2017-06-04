ebf07dd81b84bc32986429871a1b7a60cab0e0ff
Ian Bell <ian.h.bell@gmail.com>
2017-06-03 19:30:40 -0600

    Add up to third derivs

--- check_vals.py
+++ check_vals.py
@@ -7,11 +7,17 @@ rhomolar = delta*AS.rhomolar_reducing()
 T = AS.T_reducing()/tau
 
 s = 0
-N = 1000000
+N = 10000
 tic = timeit.default_timer()
 for i in range(N):
     AS.specify_phase(CP.iphase_gas)
     AS.update(CP.DmolarT_INPUTS, rhomolar, T)
-    s += AS.alphar() + AS.delta()*AS.dalphar_dDelta() + AS.tau()*AS.dalphar_dTau()
+    # s += 0
+    s += (AS.alphar() 
+          + AS.delta()*AS.dalphar_dDelta() + AS.tau()*AS.dalphar_dTau() 
+          + AS.tau()**2*AS.d2alphar_dTau2() + AS.delta()*AS.tau()*AS.d2alphar_dDelta_dTau() + AS.delta()**2*AS.d2alphar_dDelta2()
+          + AS.delta()**3*AS.d3alphar_dDelta3() + AS.delta()*AS.tau()**2*AS.d3alphar_dDelta_dTau2() + AS.delta()**2*AS.tau()*AS.d3alphar_dDelta2_dTau() + AS.tau()**3*AS.d3alphar_dTau3()
+          )
+
 toc = timeit.default_timer()
 print(s/N, (toc-tic)/N*1e6)
--- main.cpp
+++ main.cpp
@@ -26,23 +26,53 @@ int main(){
     Eigen::Map<const Eigen::VectorXd> beta_w(&(_beta[0]), _beta.size()), epsilon_w(&(_epsilon[0]), _epsilon.size()),
                                       eta_w(&(_eta[0]), _eta.size()), gamma_w(&(_gamma[0]), _gamma.size());
     
-    long N = 10000000; double summer = 0;
+    long N = 1000000; double summer = 0;
+    Eigen::ArrayXd tau_du_dtau, B1delta, delta_dB1delta_ddelta, B1tau, B2delta, B3delta, armat, umat, delta_minus_epsilon, tau_minus_gamma, delta_to_l, delta_du_ddelta, delta2_d2u_ddelta2, delta3_d3u_ddelta3, delta2_d2B1delta_ddelta2, tau2_d2u_dtau2, tau3_d3u_dtau3, tau_dB1tau_dtau, B2tau, tau2_d2B1tau_dtau2, B3tau;
     auto startTime = std::chrono::system_clock::now();
     
     for (auto i = 0; i < N; ++i){
-        const double tau = 0.8, delta = 1.3, log2tau = log2(tau), log2delta = log2(delta);
-        Eigen::ArrayXd delta_minus_epsilon = delta-epsilon_w.array(), tau_minus_gamma = tau-gamma_w.array(), delta_to_l = Eigen::pow(delta, l_w.array());
+        const double tau = 0.8 + i*0, delta = 1.3, log2tau = log2(tau), log2delta = log2(delta);
+        delta_minus_epsilon = delta-epsilon_w.array(), tau_minus_gamma = tau-gamma_w.array(), delta_to_l = Eigen::pow(delta, l_w.array());
         
-        Eigen::VectorXd umat = -c_w.array()*delta_to_l - eta_w.array()*delta_minus_epsilon.square() - beta_w.array()*tau_minus_gamma.square();
-        Eigen::VectorXd delta_du_ddelta = -c_w.array()*l_w.array()*delta_to_l -2*delta*eta_w.array()*delta_minus_epsilon;
-        Eigen::VectorXd tau_du_dtau = -2*tau*beta_w.array()*tau_minus_gamma;
+        umat = -c_w.array()*delta_to_l - eta_w.array()*delta_minus_epsilon.square() - beta_w.array()*tau_minus_gamma.square();
         
-        Eigen::VectorXd armat = n_w.array()*Eigen::pow(2, (t_w*log2tau + d_w*log2delta + umat/M_LN2).array());
+        delta_du_ddelta = -c_w.array()*l_w.array()*delta_to_l -2*delta*eta_w.array()*delta_minus_epsilon;
+        delta2_d2u_ddelta2 = -c_w.array()*l_w.array()*(l_w.array()-1)*delta_to_l -2*(delta*delta)*eta_w.array();
+        delta3_d3u_ddelta3 = -c_w.array()*l_w.array()*(l_w.array()-1)*(l_w.array()-2)*delta_to_l;
+        
+        tau_du_dtau = -2*tau*beta_w.array()*tau_minus_gamma;
+        tau2_d2u_dtau2 = -2*(tau*tau)*beta_w.array();
+        tau3_d3u_dtau3 = 0*tau2_d2u_dtau2;
+        
+        B1tau = tau_du_dtau + t_w.array();
+        // --
+        tau_dB1tau_dtau = tau2_d2u_dtau2 + tau_du_dtau;
+        B2tau = tau_dB1tau_dtau + B1tau.square() - B1tau;
+        // --
+        tau2_d2B1tau_dtau2 = tau3_d3u_dtau3 + 2*tau2_d2u_dtau2;
+        B3tau = tau2_d2B1tau_dtau2 + 3*B1tau*tau_dB1tau_dtau - 2*tau_dB1tau_dtau + B1tau.cube() - 3*B1tau.square() + 2*B1tau;
+        
+        B1delta = delta_du_ddelta + d_w.array();
+        // --
+        delta_dB1delta_ddelta = delta2_d2u_ddelta2 + delta_du_ddelta;
+        B2delta = delta_dB1delta_ddelta + B1delta.square() - B1delta;
+        // --
+        delta2_d2B1delta_ddelta2 = delta3_d3u_ddelta3 + 2*delta2_d2u_ddelta2;
+        B3delta = delta2_d2B1delta_ddelta2 + 3*B1delta*delta_dB1delta_ddelta - 2*delta_dB1delta_ddelta + B1delta.cube() - 3*B1delta.square() + 2*B1delta;
+        
+        armat = n_w.array()*Eigen::pow(2, (t_w.array()*log2tau + d_w.array()*log2delta + umat/M_LN2).array());
         
         double A00 = armat.sum();
-        double A01 = (armat.array()*(delta_du_ddelta.array() + d_w.array())).sum();
-        double A10 = (armat.array()*(tau_du_dtau.array() + t_w.array())).sum();
-        summer += A00 + A01 + A10;
+        double A10 = (armat*B1tau).sum();
+        double A01 = (armat*B1delta).sum();
+        double A20 = (armat*B2tau).sum();
+        double A11 = (armat*B1tau*B1delta).sum();
+        double A02 = (armat*B2delta).sum();
+        double A30 = (armat*B3tau).sum();
+        double A21 = (armat*B2tau*B1delta).sum();
+        double A12 = (armat*B1tau*B2delta).sum();
+        double A03 = (armat*B3delta).sum();
+        summer += A00 + A10 + A01 + A20 + A11 + A02 + A30 + A21 + A12 + A03;
     }
     auto elap = std::chrono::duration<double>(std::chrono::system_clock::now() - startTime).count()/static_cast<double>(N)*1e6;
     
