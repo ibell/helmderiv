0b0b87c55d0dbc1102bcb8d8a0bae5fc44bcc9dd
Ian Bell <ian.h.bell@gmail.com>
2017-06-10 19:38:37 -0600

    Couple more optimizations to the structure

--- Helm.h
+++ Helm.h
@@ -1,74 +1,186 @@
 
-
-#include <cmath>
-
 #include "Eigen/Dense"
+#include <cmath>
 
 #ifndef M_LN2
 #define M_LN2 0.693147180559945309417
 #endif
 
+const double one_over_M_LN2 = 1.0/M_LN2;
+
 struct GenHelmDerivCoeffs{
     Eigen::ArrayXd n,t,d,l,c,cd,ld,lt,ct,beta,epsilon,eta,gamma;
+    Eigen::ArrayXi ld_as_int;
 };
+
 struct GenHelmDerivDerivs{
     double A00,A10,A01,A20,A11,A02,A30,A21,A12,A03;
 };
 
-class GenHelmDeriv{
+// From https://stackoverflow.com/a/5625446/1360263
+template <typename T>
+T powi(T p, unsigned q)
+{
+    T r(1);
+    
+    while (q != 0) {
+        if (q % 2 == 1) {    // q is odd
+            r *= p;
+            q--;
+        }
+        p *= p;
+        q /= 2;
+    }
+    
+    return r;
+}
 
+class GenHelmDeriv{
+    
 private:
+    bool use_tau_lt = false, use_gauss_tau = true;
+    bool use_delta_ld = true, use_gauss_delta = true;
+    bool some_u;
+    
     GenHelmDerivCoeffs coeffs;
     // Work arrays
     Eigen::ArrayXd tau_du_dtau, B1delta, delta_dB1delta_ddelta, B1tau, B2delta, B3delta, armat, umat, delta_minus_epsilon, tau_minus_gamma, delta_to_ld, tau_to_lt, delta_du_ddelta, delta2_d2u_ddelta2, delta3_d3u_ddelta3, delta2_d2B1delta_ddelta2, tau2_d2u_dtau2, tau3_d3u_dtau3, tau_dB1tau_dtau, B2tau, tau2_d2B1tau_dtau2, B3tau;
-
+    
+    // Boolean mask arrays
+    Eigen::Array<bool,Eigen::Dynamic,1> mask_gauss_delta,mask_delta_ld,mask_gauss_tau,mask_tau_lt;
+    
+    void init(){
+        some_u = use_tau_lt || use_gauss_tau || use_delta_ld || use_gauss_delta;
+        mask_gauss_delta = Eigen::Array<bool,Eigen::Dynamic,1>::Constant(coeffs.n.size(), use_gauss_delta);
+        mask_delta_ld = Eigen::Array<bool,Eigen::Dynamic,1>::Constant(coeffs.n.size(), use_delta_ld);
+        mask_gauss_tau = Eigen::Array<bool,Eigen::Dynamic,1>::Constant(coeffs.n.size(), use_gauss_tau);
+        mask_tau_lt = Eigen::Array<bool,Eigen::Dynamic,1>::Constant(coeffs.n.size(), use_tau_lt);
+    };
+    
 public:
-    GenHelmDeriv(GenHelmDerivCoeffs &&coeffs) : coeffs(coeffs){};
-    GenHelmDeriv(const GenHelmDerivCoeffs &coeffs) : coeffs(coeffs){};
-
-    void calc(const double tau, const double delta, GenHelmDerivDerivs &derivs)
+    bool all_ld_integers = true;
+    GenHelmDeriv(GenHelmDerivCoeffs &&coeffs) : coeffs(coeffs){ init(); };
+    GenHelmDeriv(const GenHelmDerivCoeffs &coeffs) : coeffs(coeffs){ init(); };
+    
+    void calc(const double tau, const double delta, GenHelmDerivDerivs &derivs, const std::size_t Ntau_max = 3, const std::size_t Ndelta_max = 3)
     {
-
         const double log2tau = log2(tau), log2delta = log2(delta);
-        delta_minus_epsilon = delta- coeffs.epsilon, tau_minus_gamma = tau- coeffs.gamma, delta_to_ld = Eigen::pow(delta, coeffs.ld), tau_to_lt = Eigen::pow(tau, coeffs.lt);
-    
-        umat = -coeffs.cd*delta_to_ld - coeffs.ct*tau_to_lt - coeffs.eta*delta_minus_epsilon.square() - coeffs.beta*tau_minus_gamma.square();
-    
-        delta_du_ddelta = -coeffs.cd*coeffs.ld*delta_to_ld -2*delta*coeffs.eta*delta_minus_epsilon;
-        delta2_d2u_ddelta2 = -coeffs.cd*coeffs.ld*(coeffs.ld-1)*delta_to_ld -2*(delta*delta)*coeffs.eta;
-        delta3_d3u_ddelta3 = -coeffs.cd*coeffs.ld*(coeffs.ld-1)*(coeffs.ld-2)*delta_to_ld;
-    
-        tau_du_dtau = -coeffs.ct*coeffs.lt*tau_to_lt  -2*tau*coeffs.beta*tau_minus_gamma;
-        tau2_d2u_dtau2 = -coeffs.ct*coeffs.lt*(coeffs.lt - 1)*tau_to_lt -2*(tau*tau)*coeffs.beta;
-        tau3_d3u_dtau3 = -coeffs.ct*coeffs.lt*(coeffs.lt - 1)*(coeffs.lt- 2)*tau_to_lt;
-    
-        B1tau = tau_du_dtau + coeffs.t;
-        // --
-        tau_dB1tau_dtau = tau2_d2u_dtau2 + tau_du_dtau;
-        B2tau = tau_dB1tau_dtau + B1tau.square() - B1tau;
-        // --
-        tau2_d2B1tau_dtau2 = tau3_d3u_dtau3 + 2*tau2_d2u_dtau2;
-        B3tau = tau2_d2B1tau_dtau2 + 3*B1tau*tau_dB1tau_dtau - 2*tau_dB1tau_dtau + B1tau.cube() - 3*B1tau.square() + 2*B1tau;
-    
-        B1delta = delta_du_ddelta + coeffs.d;
-        // --
-        delta_dB1delta_ddelta = delta2_d2u_ddelta2 + delta_du_ddelta;
-        B2delta = delta_dB1delta_ddelta + B1delta.square() - B1delta;
-        // --
-        delta2_d2B1delta_ddelta2 = delta3_d3u_ddelta3 + 2*delta2_d2u_ddelta2;
-        B3delta = delta2_d2B1delta_ddelta2 + 3*B1delta*delta_dB1delta_ddelta - 2*delta_dB1delta_ddelta + B1delta.cube() - 3*B1delta.square() + 2*B1delta;
-    
-        armat = coeffs.n*Eigen::pow(2, (coeffs.t*log2tau + coeffs.d*log2delta + umat/M_LN2).array());
-    
+        
+        // Gaussian difference vectors
+        if (use_gauss_delta) { delta_minus_epsilon = delta - coeffs.epsilon; }
+        if (use_gauss_tau){ tau_minus_gamma = tau - coeffs.gamma; }
+        
+        if (use_delta_ld) {
+            if (all_ld_integers){
+                double _delta = delta;
+                delta_to_ld = coeffs.ld_as_int.unaryExpr( [_delta](int _i){return powi(_delta, _i);} );
+            }
+            else{
+                delta_to_ld = Eigen::pow(delta, coeffs.ld);
+            }
+        }
+        
+        if (use_tau_lt) { tau_to_lt = Eigen::pow(tau, coeffs.lt); }
+        
+        if (some_u){
+            umat = (
+                mask_gauss_delta.select(-coeffs.eta*delta_minus_epsilon.square(),0)
+                +
+                mask_delta_ld.select(-coeffs.cd*delta_to_ld,0)
+                +
+                mask_gauss_tau.select(-coeffs.beta*tau_minus_gamma.square(),0)
+                +
+                mask_tau_lt.select(-coeffs.ct*tau_to_lt,0)
+            );
+        }
+        else{
+            umat = 0*coeffs.n;
+        }
+        
+        // *******************************************************************
+        // *******************************************************************
+        //                               TAU TERMS
+        // *******************************************************************
+        // *******************************************************************
+        
+        if (Ntau_max >= 1){
+            if(use_gauss_tau && use_tau_lt){ tau_du_dtau = -2*tau*coeffs.beta*tau_minus_gamma -coeffs.ct*coeffs.lt*tau_to_lt; }
+            else if(use_tau_lt){ tau_du_dtau = -coeffs.ct*coeffs.lt*tau_to_lt; }
+            else if(use_gauss_tau){ tau_du_dtau = -2*tau*coeffs.beta*tau_minus_gamma; }
+            else{
+                tau_du_dtau = 0*coeffs.n;
+            }
+            B1tau = tau_du_dtau + coeffs.t;
+        }
+        if (Ntau_max >= 2){
+            if (use_gauss_tau && use_tau_lt){ tau2_d2u_dtau2 = -2*(tau*tau)*coeffs.beta -coeffs.ct*coeffs.lt*(coeffs.lt - 1)*tau_to_lt; }
+            else if(use_tau_lt){ tau2_d2u_dtau2 = -coeffs.ct*coeffs.lt*(coeffs.lt - 1)*tau_to_lt; }
+            else if(use_gauss_tau){ tau2_d2u_dtau2 = -2*(tau*tau)*coeffs.beta; }
+            else{
+                tau2_d2u_dtau2 = 0*coeffs.n;
+            }
+            tau_dB1tau_dtau = tau2_d2u_dtau2 + tau_du_dtau;
+            B2tau = tau_dB1tau_dtau + B1tau.square() - B1tau;
+        }
+        if (Ntau_max >= 3){
+            if (use_tau_lt){
+                tau3_d3u_dtau3 = -coeffs.ct*coeffs.lt*(coeffs.lt - 1)*(coeffs.lt- 2)*tau_to_lt;
+            }
+            else{
+                tau3_d3u_dtau3 = 0*coeffs.n;
+            }
+            tau2_d2B1tau_dtau2 = tau3_d3u_dtau3 + 2*tau2_d2u_dtau2;
+            B3tau = tau2_d2B1tau_dtau2 + 3*B1tau*tau_dB1tau_dtau - 2*tau_dB1tau_dtau + B1tau.cube() - 3*B1tau.square() + 2*B1tau;
+        }
+        
+        // *******************************************************************
+        // *******************************************************************
+        //                               DELTA TERMS
+        // *******************************************************************
+        // *******************************************************************
+        
+        if (Ndelta_max >= 1){
+            if (use_gauss_delta && use_delta_ld){ delta_du_ddelta = -2*delta*coeffs.eta*delta_minus_epsilon -coeffs.cd*coeffs.ld*delta_to_ld; }
+            else if (use_delta_ld){ delta_du_ddelta = -coeffs.cd*coeffs.ld*delta_to_ld; }
+            else if (use_gauss_delta) { delta_du_ddelta = -2*delta*coeffs.eta*delta_minus_epsilon; }
+            else{
+                delta_du_ddelta = 0*coeffs.n;
+            }
+            B1delta = delta_du_ddelta + coeffs.d;
+        }
+        if (Ndelta_max >= 2){
+            if (use_gauss_delta && use_delta_ld){ delta2_d2u_ddelta2 = -2*(delta*delta)*coeffs.eta -coeffs.cd*coeffs.ld*(coeffs.ld-1)*delta_to_ld; }
+            else if (use_delta_ld) { delta2_d2u_ddelta2 = -coeffs.cd*coeffs.ld*(coeffs.ld-1)*delta_to_ld; }
+            else if (use_gauss_delta) { delta2_d2u_ddelta2 = -2*(delta*delta)*coeffs.eta; }
+            else{
+                delta2_d2u_ddelta2 = 0*coeffs.n;
+            }
+            delta_dB1delta_ddelta = delta2_d2u_ddelta2 + delta_du_ddelta;
+            B2delta = delta_dB1delta_ddelta + B1delta.square() - B1delta;
+        }
+        if (Ndelta_max >= 3){
+            if (use_delta_ld){
+                delta3_d3u_ddelta3 = -coeffs.cd*coeffs.ld*(coeffs.ld-1)*(coeffs.ld-2)*delta_to_ld;
+            }
+            else{
+                delta3_d3u_ddelta3 = 0*coeffs.n;
+            }
+            delta2_d2B1delta_ddelta2 = delta3_d3u_ddelta3 + 2*delta2_d2u_ddelta2;
+            B3delta = delta2_d2B1delta_ddelta2 + 3*B1delta*delta_dB1delta_ddelta - 2*delta_dB1delta_ddelta + B1delta.cube() - 3*B1delta.square() + 2*B1delta;
+        }
+        
+        // Finally, carry out the calculations
+        
+        armat = coeffs.n*Eigen::pow(2, (coeffs.t*log2tau + coeffs.d*log2delta + umat*one_over_M_LN2).array());
         derivs.A00 = armat.sum();
-        derivs.A10 = (armat*B1tau).sum();
-        derivs.A01 = (armat*B1delta).sum();
-        derivs.A20 = (armat*B2tau).sum();
-        derivs.A11 = (armat*B1tau*B1delta).sum();
-        derivs.A02 = (armat*B2delta).sum();
-        derivs.A30 = (armat*B3tau).sum();
-        derivs.A21 = (armat*B2tau*B1delta).sum();
-        derivs.A12 = (armat*B1tau*B2delta).sum();
-        derivs.A03 = (armat*B3delta).sum();
+        derivs.A10 = (Ntau_max >=1) ? (armat*B1tau).sum() : 0;
+        derivs.A01 = (Ndelta_max >= 1) ? (armat*B1delta).sum() : 0;
+        derivs.A20 = (Ntau_max >=2) ? (armat*B2tau).sum() : 0;
+        derivs.A11 = (Ntau_max >= 1 && Ndelta_max >= 1) ? (armat*B1tau*B1delta).sum() : 0;
+        derivs.A02 = (Ndelta_max >=2) ? (armat*B2delta).sum() : 0;
+        derivs.A30 = (Ntau_max >=3) ? (armat*B3tau).sum() : 0;
+        derivs.A21 = (Ntau_max >= 2 && Ndelta_max >=1) ? (armat*B2tau*B1delta).sum() : 0;
+        derivs.A12 = (Ntau_max >= 1 && Ndelta_max >=2) ? (armat*B1tau*B2delta).sum() : 0;
+        derivs.A03 = (Ndelta_max >=2) ? (armat*B3delta).sum() : 0;
     };
 };
--- main.cpp
+++ main.cpp
@@ -4,13 +4,17 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <cmath>
+#include <iomanip>
 
 int main(){
+    
     GenHelmDerivCoeffs coeff;
     coeff.d.resize(18); coeff.d << 4.0,1.0,1.0,2.0,2.0,1.0,3.0,6.0,6.0,2.0,3.0,1.0,1.0,1.0,2.0,2.0,4.0,1.0;
     coeff.n.resize(18); coeff.n << 0.042910051,1.7313671,-2.4516524,0.34157466,-0.46047898,-0.66847295,0.20889705,0.19421381,-0.22917851,-0.60405866,0.066680654,0.017534618,0.33874242,0.22228777,-0.23219062,-0.09220694,-0.47575718,-0.017486824;
     coeff.t.resize(18); coeff.t << 1,0.33,0.8,0.43,0.9,2.46,2.09,0.88,1.09,3.25,4.62,0.76,2.5,2.75,3.05,2.55,8.4,6.75;
     coeff.ld.resize(18); coeff.ld << 0.,0,0,0,0,1,1,1,1,2,2,0,0,0,0,0,0,0;
+    coeff.ld_as_int.resize(18); coeff.ld_as_int << 0.,0,0,0,0,1,1,1,1,2,2,0,0,0,0,0,0,0;
     coeff.cd.resize(18); coeff.cd << 0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0;
     coeff.lt.resize(18); coeff.lt << 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0;
     coeff.ct.resize(18); coeff.ct << 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0;
@@ -23,13 +27,29 @@ int main(){
     auto startTime = std::chrono::system_clock::now();
     GenHelmDeriv gen(coeff);
     GenHelmDerivDerivs d;
-    long N = 1000000; double summer = 0;
-    for (auto i = 0; i < N; ++i){
-        gen.calc(0.8, 1.3, d);
-        summer += d.A00 + d.A10 + d.A01 + d.A20 + d.A11 + d.A02 + d.A30 + d.A21 + d.A12 + d.A03;
-    }
-    auto elap = std::chrono::duration<double>(std::chrono::system_clock::now() - startTime).count()/static_cast<double>(N)*1e6;
-    std::cout << summer/N << " " << elap << std::endl;
-    return EXIT_SUCCESS;
+    
+    long N = 10000000; double summer = 0;
+    
+    for (auto ld_int : {true, false}){
+        summer = 0;
+        gen.all_ld_integers = ld_int;
+        startTime = std::chrono::system_clock::now();
+        for (auto i = 0; i < N; ++i){
+            gen.calc(0.8, 1.3e-2, d);
+            summer += d.A00 + d.A10 + d.A01 + d.A20 + d.A11 + d.A02 + d.A30 + d.A21 + d.A12 + d.A03;
+        }
+        auto elap = std::chrono::duration<double>(std::chrono::system_clock::now() - startTime).count()/static_cast<double>(N)*1e6;
+        std::cout << std::setprecision(20) << summer/N << " " << elap << std::endl;
 
+        summer = 0;
+        gen.all_ld_integers = ld_int;
+        startTime = std::chrono::system_clock::now();
+        for (auto i = 0; i < N; ++i){
+            gen.calc(0.8, 1.3e-2, d, 1, 2);
+            summer += d.A00 + d.A10 + d.A01 + d.A20 + d.A11 + d.A02 + d.A30 + d.A21 + d.A12 + d.A03;
+        }
+        elap = std::chrono::duration<double>(std::chrono::system_clock::now() - startTime).count()/static_cast<double>(N)*1e6;
+        std::cout << summer/N << " " << elap << std::endl;
+    }
+    return EXIT_SUCCESS;
 }
